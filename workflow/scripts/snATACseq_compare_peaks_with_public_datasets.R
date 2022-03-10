456c01fc5df4a2639cf2f69a2fb6fa24d8a76fdc
Dazcam <Darren@Darrens-MacBook-Pro.local>
2022-03-10 16:59:43 +0000

    Jaccard pairwise peak tests

--- workflow/scripts/snATACseq_compare_peaks_with_public_datasets.R
+++ workflow/scripts/snATACseq_compare_peaks_with_public_datasets.R
@@ -18,16 +18,18 @@
 library(readxl)
 library(tidyverse)
 library(bedr)
-#library(Repitools)
+
 
 ##  Define global variables  -----------------------------------------------------------
 cat('\nDefining variables ... \n')
-IN_DIR <- "~/Desktop/fetal_brain_snATACseq_070222/resources/sheets/"
 PEAK_DIR <- "~/Desktop/fetal_brain_snATACseq_070222/results/archR_data_processing/peaks/"
 ZIFFRA_DIR <- "~/Desktop/fetal_brain_snATACseq_070222/resources/public_datasets/ziffra_2021/"
+OUT_DIR <- "~/Desktop/fetal_brain_snATACseq_070222/results/"
 REGIONS <- c("FC", "GE")
 CELL_TYPES <- c("FC.ExN", "FC.InN", "FC.RG", "FC.MG", "FC.undef", "LGE.InN", "MGE.InN", "CGE.InN", "GE.RG", "GE.Proj")
-ZIFFRA_CELL_TYPES <- c()
+ZIFFRA_CELL_TYPES <- c("AstroOligo", "dlEN", "earlyEN", "EndoMural", "IN_CGE", 
+                       "IN_MGE", "IPC", "Microglia", "RG", "ulEN", 
+                       "MGE", "Insula")
 TOKEN <- "" # Needed for LDlinkR - saved in email
 
 ##  Load public data  -----------------------------------------------------------------
@@ -71,25 +73,23 @@ ziffra_peaks_specific <- specific_peak_list %>%
   filter(val == 1) %>%
   group_split(cell_type)
 
+# Sort my bed files for Jaccard testing
+for (CELL_TYPE in CELL_TYPES) {
+  
+  cat('\n\n Checking if ', CELL_TYPE, ' is valid and sorted\n\n')
+  MY_PEAKS <- get(paste0(CELL_TYPE, "_peaks"))
+  MY_PEAKS <- bedr.sort.region(MY_PEAKS) # Sorts the validates
+  is.sorted <- is.sorted.region(MY_PEAKS, method = "lexicographical", engine = 'R')
+  assign(paste0(CELL_TYPE, '_peaks_srtd'), MY_PEAKS)
+  
+}
 
-
-
-
-# 
-zif_test <- ziffra_peaks_all[1:50,] %>%
-  unite("peaks", chr:start, sep = ':') %>%
-  unite("peaks", peaks:end, sep = '-') %>%
-  select(peaks) %>%
-  pull()
-
-zif_test <- bedr.sort.region(zif_test, method = "lexicographical", engine ='R')
-zif_test <- bedr.merge.region(zif_test)
 
 # Note efficient - pull my cell type out of loop as it's running the checks for those multiple times
 for (i in 1:length(ziffra_peaks_all)) {
   
   ZIFFRA_PEAKS <- ziffra_peaks_all[[i]]
-  ZIFFRA_CELL_TYPE <- unique(PEAKS$cell_type)
+  ZIFFRA_CELL_TYPE <- unique(ZIFFRA_PEAKS$cell_type)
   
   ZIFFRA_PEAKS <- ZIFFRA_PEAKS %>% 
     unite("peaks", chr:start, sep = ':') %>%
@@ -101,27 +101,48 @@ for (i in 1:length(ziffra_peaks_all)) {
   cat('\n\n Checking if ', ZIFFRA_CELL_TYPE, ' is valid and sorted\n\n')
   ZIFFRA_PEAKS <- bedr.sort.region(ZIFFRA_PEAKS, method = "lexicographical", engine ='R')
   ZIFFRA_PEAKS <- bedr.merge.region(ZIFFRA_PEAKS)
+  assign(paste0(ZIFFRA_CELL_TYPE, '_peaks_srtd'), ZIFFRA_PEAKS)
   
-  for (CELL_TYPE in CELL_TYPES) {
-    
-    cat('\n\n Checking if ', CELL_TYPE, ' is valid and sorted\n\n')
-    MY_PEAKS <- get(paste0(CELL_TYPE, "_peaks"))
-    MY_PEAKS <- bedr.sort.region(MY_PEAKS) # Sorts the validates
-    is.sorted <- is.sorted.region(MY_PEAKS, method = "lexicographical", engine = 'R')
-    
-    cat('\n\n Running jaccard on  ', CELL_TYPE, ' and ', ZIFFRA_CELL_TYPE,' \n\n')
-    jaccard.stats <- jaccard(MY_PEAKS, ZIFFRA_PEAKS)
-    
-    cat('\n\n Assigning files ... \n\n')
-    assign(paste0(CELL_TYPE, '_peaks_srtd'), PEAKS)
-    assign(paste0(CELL_TYPE, '_', ZIFFRA_CELL_TYPE, '_jaccard'), jaccard.stats)
-    
-  }
-
 }
 
+# Create df for pairwise tests
+pairwise_df <- data.frame(bray  = c("FC.ExN", "FC.ExN", "FC.ExN", "FC.InN", "FC.RG", 
+                                     "FC.MG", "FC.undef", "LGE.InN", "MGE.InN", "MGE.InN",
+                                      "CGE.InN", "GE.RG", "GE.Proj"),
+                          ziffra = c("earlyEN", "dlEN", "ulEN", "IN_MGE", "RG",
+                                    "Microglia", "EndoMural", "IN_MGE", "IN_MGE", "MGE",
+                                    "IN_CGE", "RG", "Insula")
+                  
+)
+
+jaccard_df <- data.frame()
+
+for (i in 1:nrow(pairwise_df)) {
+  
+  BRAY_cell_type <- pairwise_df[i, 1]
+  ZIFFRA_cell_type <- pairwise_df[i, 2]
+  BRAY_peaks <- get(paste0(BRAY_cell_type, '_peaks_srtd'))
+  ZIFFRA_peaks <- get(paste0(ZIFFRA_cell_type, '_peaks_srtd'))
+  print(length(BRAY_peaks))
+  print(length(ZIFFRA_peaks))
+  
+  cat('\nRunning Jaccard test for ', BRAY_cell_type, ' and ', ZIFFRA_cell_type, '\n')
+  jaccard.stats <- jaccard(BRAY_peaks, ZIFFRA_peaks)
+  jaccard_df <- rbind(jaccard_df, jaccard.stats)
+  cat('\n\n Assigning files ... \n\n')
+  
+  assign(paste0(BRAY_cell_type, '_', ZIFFRA_cell_type, '_jaccard'), jaccard.stats)
+  
+}
+
+pairwise_table <- cbind(pairwise_df, jaccard_df)
+rownames(pairwise_table) <- NULL
+
+write.table(pairwise_table, paste0(OUT_DIR, "ziffra_peak_comparison_jaccard_tests.tsv"), 
+            quote = FALSE,sep = '\t', row.names = FALSE, col.names = TRUE)
 
 
 
-reldist.stats <- reldist(FC.ExN_peaks, zif_test)
+
+
 
